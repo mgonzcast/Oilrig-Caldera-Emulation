d6452f2ab4a32cb10d0afb66511e7e41b16e605d
mgonzcast <63349279+mgonzcast@users.noreply.github.com>
2025-10-23 18:34:13 +0200

    Create Create_groups_and_users.ps1

---
+++ setup-lab/Create_groups_and_users.ps1
@@ -0,0 +1,31 @@
+Rename-Computer diskjockey
+add-windowsfeature ad-domain-services -includemanagementtools
+
+New-ADGroup 'EWS Admins' -GroupScope Global
+New-ADGroup 'SQL Admins' -Groupscope Global
+
+$path="CN=Users,DC=boombox,DC=com"
+
+### Hay que meter el principal name (logon)
+
+New-ADUser -Name gosta -UserPrincipalName gosta@boombox.com -Path $path -Enabled $True `
+-AccountPassword $(ConvertTo-SecureString 'd0ntGoCH4ingW8trfalls' -AsPlainText -Force) -passThru
+
+Add-ADGroupMember -Identity "EWS Admins" -Members gosta
+
+New-ADUser -Name tous -UserPrincipalName tous@boombox.com -Path $path -Enabled $True `
+-AccountPassword $(ConvertTo-SecureString 'd0ntGoCH4ingW8trfalls' -AsPlainText -Force) -passThru
+
+Add-ADGroupMember -Identity "EWS Admins" -Members tous
+Add-ADGroupMember -Identity "SQL Admins" -Members tous
+
+New-ADUser -Name mariam -UserPrincipalName mariam@boombox.com -Path $path -Enabled $True `
+-AccountPassword $(ConvertTo-SecureString 'd0ntGoCH4ingW8trfalls' -AsPlainText -Force) -passThru
+ 
+New-ADUser -Name shiroyeh -UserPrincipalName shiroyeh@boombox.com -Path $path -Enabled $True `
+-AccountPassword $(ConvertTo-SecureString 'd0ntGoCH4ingW8trfalls' -AsPlainText -Force) -passThru
+
+New-ADUser -Name shiroyeh_admin -UserPrincipalName shiroyeh_admin@boombox.com -Path $path -Enabled $True `
+-AccountPassword $(ConvertTo-SecureString 'd0ntGoCH4ingW8trfalls' -AsPlainText -Force) -passThru
+
+Add-ADGroupMember -Identity "Domain Admins" -Members shiroyeh_admin
