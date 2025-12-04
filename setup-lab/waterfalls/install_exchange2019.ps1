90441f413fdd3b3594e69a563c4ea943c95de104
mgonzcast <63349279+mgonzcast@users.noreply.github.com>
2025-12-04 13:42:33 +0100

    Update install_exchange2019.ps1

--- setup-lab/waterfalls/install_exchange2019.ps1
+++ setup-lab/waterfalls/install_exchange2019.ps1
@@ -9,5 +9,12 @@ cd D:\
 # Migration and Post Above Install
 .\Setup.exe /m:install /roles:m /IAcceptExchangeServerLicenseTerms_DiagnosticDataOFF /InstallWindowsComponents 
 
+set-owavirtualdirectory -Identity "owa (Default Web Site)" -ExternalURL https://waterfalls.boombox.com/owa -InternalURL https://waterfalls.boombox.com/owa
+
+set-ecpvirtualdirectory -Identity "ecp (Default Web Site)" -ExternalURL https://waterfalls.boombox.com/ecp -InternalURL https://waterfalls.boombox.com/ecp
+
+Set-MapiVirtualDirectory -Identity "WATERFALLS\mapi (Default Web Site)" -IISAuthenticationMethods NTLM,Negotiate
+
+
 # Launch the Exchange Management Shell
 LaunchEMS
