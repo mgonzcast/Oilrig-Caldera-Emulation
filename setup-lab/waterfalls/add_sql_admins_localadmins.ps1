e4dd7b73035ece4ee92e93bc50c2b1784ee0a7fc
mgonzcast <63349279+mgonzcast@users.noreply.github.com>
2026-01-15 16:39:47 +0100

    Add files via upload

---
+++ setup-lab/waterfalls/add_sql_admins_localadmins.ps1
@@ -0,0 +1,8 @@
+Add-LocalGroupMember -Group administrators -member "BOOMBOX\EWS Admins"
+
+Write-Host "";Write-Host "Adding EWS Admins to Local Administrators"
+
+# Check EWS admins were added
+
+Get-LocalGroupMember administrators
+
